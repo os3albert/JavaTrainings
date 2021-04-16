945f2617b1e20993dd4675732b47e58460a71505
Albert <47365826+os3albert@users.noreply.github.com>
2021-04-16 14:31:23 +0200

    nothing special

---
+++ Prova/src/Ciao.java
@@ -0,0 +1,9 @@
+
+public class Ciao {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- Prova/src/Main.java
+++ Prova/src/Main.java
@@ -1,11 +1,17 @@
+import java.awt.Rectangle;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println(1);
-		
+		Rectangle box = new Rectangle(0, 0, 20, 20);	
+		System.out.println(box);
+		System.out.println(box.height);
+		box.translate(15, 25);
+		System.out.println(box);
+		box.setSize(20, 40);
+		System.out.println(box);
 		
 		
 	}
